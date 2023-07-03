5be690764f7b4cf342c0ad9b4cd2888216c783ba
Elvis Sunguti <elviskendrick99@gmail.com>
2023-07-03 10:40:44 +0300

    added backend features

---
+++ Server.js
@@ -0,0 +1,43 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+mongoose.connect(
+    'mongodb://dj-mixes:dj-mixes@ac-xhkvbmr-shard-00-00.e4ffdhs.mongodb.net:27017,ac-xhkvbmr-shard-00-01.e4ffdhs.mongodb.net:27017,ac-xhkvbmr-shard-00-02.e4ffdhs.mongodb.net:27017/dj-mixes?ssl=true&replicaSet=atlas-7xjnul-shard-0&authSource=admin&retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true}
+    ).then(() => {
+        console.log("Connected to mongodb atlas!");
+    }).catch((err) => {
+        console.log("Error connecting to MongoDb atlas!", err)
+    });
+
+
+    app.use((req, res, next) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+        );
+        res.setHeader(
+          "Access-Control-Allow-Methods",
+          "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+        );
+        next();
+      });
+
+      
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT} `)
+});
+    
---
+++ src/Components/CurrentSong/CurrentSong.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CurrentSong = () => {
+    return(
+        <section>
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
+export default CurrentSong;
