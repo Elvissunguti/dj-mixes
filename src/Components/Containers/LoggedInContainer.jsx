693c0da8f7e3793bc28b2478e6f269f94801ff99
Elvis Sunguti <elviskendrick99@gmail.com>
2023-07-31 10:32:46 +0300

    fixed bugs

--- src/Components/Containers/LoggedInContainer.jsx
+++ src/Components/Containers/LoggedInContainer.jsx
@@ -22,42 +22,42 @@ const LoggedInContainer = ({ children, curActiveScreen, }) => {
     return(
         <section>
             <NavBar />
-            <div className="mx-auto ">
-                <div className="h-full w-1/5 flex flex-col pb-10  ">
+            <div className="mx-auto flex">
+                <div className="h-full w-1/5 flex flex-col  pb-10  ">
                 <ul className="flex flex-col px-6 space-y-6 ">
                         <li>
-                            <Link to="/feed" onClick={() => handleClick(0)} active={curActiveScreen === "feed"} className={`flex items-center space-x-4 cursor-pointer ${active === 0 ? "text-green-500" : "text-blue-400"}`}>
+                            <Link to="/feed" onClick={() => handleClick(0)}  className={`flex items-center space-x-4 cursor-pointer ${active === 0 || curActiveScreen === "feed" ? "text-green-500" : "text-blue-400"}`}>
                                 <FiLayers /><p>FEED</p></Link>
                         </li>
                         <li>
-                            <Link to="/new uploads" onClick={() => handleClick(1)} active={curActiveScreen === "new uploads"} className={`flex items-center space-x-4 cursor-pointer ${active === 1 ? "text-green-500" : "text-blue-400"}`}>
+                            <Link to="/new uploads" onClick={() => handleClick(1)} className={`flex items-center space-x-4 cursor-pointer ${active === 1 || curActiveScreen === "new uploads" ? "text-green-500" : "text-blue-400"}`}>
                                 <GoVideo  /><p>NEW UPLOADS</p></Link>
                             </li>
                         <li>
-                            <Link to="/my mixes" onClick={() => handleClick(2)} active={curActiveScreen === "my mixes"} className={`flex items-center space-x-4 cursor-pointer ${active === 2 ? "text-green-500" : "text-blue-400"}`}>
-                                <FiMusic /><p>MY Mixes</p></Link>
+                            <Link to="/my mixes" onClick={() => handleClick(2)}  className={`flex items-center space-x-4 cursor-pointer ${active === 2 || curActiveScreen === "my mixes" ? "text-green-500" : "text-blue-400"}`}>
+                                <FiMusic /><p>MY MIXES</p></Link>
                         </li>
                         <li>
-                            <Link to="/post page" onClick={() => handleClick(3)} active={curActiveScreen === "post page"} className={`flex items-center space-x-4 cursor-pointer ${active === 3 ? "text-green-500" : "text-blue-400"}`}>
+                            <Link to="/post page" onClick={() => handleClick(3)}  className={`flex items-center space-x-4 cursor-pointer ${active === 3 || curActiveScreen === "post page" ? "text-green-500" : "text-blue-400"}`}>
                                 <MdOutlineLibraryBooks /><p>POSTS</p></Link>
                         </li>
                         <li>
-                            <Link to="/favourites" onClick={() => handleClick(4)} active={curActiveScreen === "favourites"} className={`flex items-center space-x-4 cursor-pointer ${active === 4 ? "text-green-500" : "text-blue-400"}`}>
+                            <Link to="/favourites" onClick={() => handleClick(4)}  className={`flex items-center space-x-4 cursor-pointer ${active || curActiveScreen === "favourites" ? "text-green-500" : "text-blue-400"}`}>
                                 <AiOutlineHeart /><p>FAVOURITES</p></Link>
                         </li>
                         <li>
-                            <Link to="/historys"onClick={() => handleClick(5)}  active={curActiveScreen === "historys"} className={`flex items-center space-x-4 cursor-pointer ${active === 5 ? "text-green-500" : "text-blue-400"}`}>
+                            <Link to="/historys"onClick={() => handleClick(5)} className={`flex items-center space-x-4 cursor-pointer ${active === 5 || curActiveScreen === "historys" ? "text-green-500" : "text-blue-400"}`}>
                                 <SlCalender /><p>HISTORY</p></Link>
                             </li>
                         <li>
-                            <Link to="/playlists" onClick={() => handleClick(6)} active={curActiveScreen === "playlists"} className={`flex items-center space-x-4 cursor-pointer ${active === 6 ? "text-green-500" : "text-blue-400"}`}>
+                            <Link to="/playlists" onClick={() => handleClick(6)} className={`flex items-center space-x-4 cursor-pointer ${active === 6 || curActiveScreen === "playlists" ? "text-green-500" : "text-blue-400"}`}>
                                 <SlPlaylist /><p>PLAYLISTS</p></Link>
                         </li>
                         </ul>
                 </div>
 
                 {/* main section */}
-                <div className="w-4/5">
+                <div className="w-4/5 overflow-auto h-full">
                     <div>
                         <div className="content p-8 pt-0 overflow-auto">
                             {children}
