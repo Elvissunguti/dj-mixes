11724dbe2006431e04c64e6acf0671dd212658a6
Elvis Sunguti <elviskendrick99@gmail.com>
2023-08-01 16:47:12 +0300

    added mix model

---
+++ src/Backend/models/Mix.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Mix = new mongoose.schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    thumbnail: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    track: {
+        type: String,
+        required: true,
+    },
+    artist: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+    },
+});
+
+const MixModel = mongoose.model("Mix", Mix);
+
+module.exports = MixModel;
+
