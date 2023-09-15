2ab296446bbe5bcdc3b8037aaf0552d674cc3920
Elvis Sunguti <elviskendrick99@gmail.com>
2023-09-15 05:57:25 +0300

    Added CurrentMix functionality to be able to play with the currentMix when a track is played

--- src/Components/My Mix/MyMix.jsx
+++ src/Components/My Mix/MyMix.jsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from "react";
 import LoggedInContainer from "../Containers/LoggedInContainer";
 import { makeAuthenticatedGETRequest, makeAuthenticatedPOSTRequest } from "../Utils/ServerHelpers";
 import MixCard from "../shared/MixCard";
+import CurrentMix from "../shared/CurrentMix";
 
 const MyMix = () => {
 
     const [mixData, setMixData ] = useState([]);
+    const [currentMix, setCurrentMix] = useState(null);
+    const [currentlyPlayingMixId, setCurrentlyPlayingMixId] = useState(null);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     useEffect(() => {
         const getData = async () => {
@@ -66,28 +70,74 @@ const MyMix = () => {
       };
 
 
+      const handlePlayPause = (mixId) => {
+        if (currentlyPlayingMixId === mixId) {
+          // Pause the currently playing mix
+          setIsPlaying(false);
+          setCurrentlyPlayingMixId(null);
+        } else {
+          // Play the selected mix
+          setIsPlaying(true);
+          setCurrentlyPlayingMixId(mixId);
+          // Find the playing mix data from feedData and set it as the currentMix
+          const playingMix = mixData.find((item) => item._id === mixId);
+          setCurrentMix({
+            ...playingMix,
+            currentSong: "play",
+            currentTime: 0,
+            duration: playingMix.trackDuration,
+          });
+        }
+      };
+
+
     return(
        <LoggedInContainer curActiveScreen="my mixes">
         <div className="flex items-start mb-6">
             <h1 className="font-bold text-xl">MY MIXES</h1>
         </div>
         <div className="space-y-4 overflow-auto  ">
-            { mixData.map((item, index) => {
-                return <MixCard 
-                         key={index}  
-                         mixId={item._id}
-                         thumbnail={item.thumbnail}  
-                         title={item.title}
-                         artist={item.artist} 
-                         userId={item.userId}
-                         audioSrc={item.track}
-                         toggleFavourite={() =>
-                            handleToggleFavourite(item._id, item.isFavourite)
-                          }
-                          favouriteCount={item.favouriteCount}
+            {mixData.length > 0 ? (
+              mixData.map((item, index) => (
+                 <MixCard 
+                    key={index}  
+                    mixId={item._id}
+                    thumbnail={item.thumbnail}  
+                    title={item.title}
+                    artist={item.artist} 
+                    userId={item.userId}
+                    audioSrc={item.track}
+                    toggleFavourite={() =>
+                    handleToggleFavourite(item._id, item.isFavourite)
+                       }
+                    favouriteCount={item.favouriteCount}
+                    onMixPlay={handlePlayPause}
+                    currentlyPlayingMixId={currentlyPlayingMixId}
+                     isPlaying={isPlaying}
                          />
-            })}
+                ))
+                ) : (
+                 <p>Loading...</p>
+              )}
         </div>
+        {currentMix && (
+          <CurrentMix
+          mixId={currentMix._id}
+          thumbnail={currentMix.thumbnail}
+          title={currentMix.title}
+          artist={currentMix.artist}
+          audioSrc={currentMix.track}
+          currentSong={currentMix.currentSong}
+          setCurrentSong={(songState) => 
+              setCurrentMix({...currentMix, currentMix: songState})
+          }
+          currentTime={currentMix.currentTime}
+          duration={currentMix.duration}
+          isPlaying={isPlaying}
+          onMixPlay={handlePlayPause}
+
+          />
+        )}
        </LoggedInContainer>
     )
 }
