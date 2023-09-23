2bc9ae074cf3cd6b547b5100659c540f59c5b963
Elvis Sunguti <elviskendrick99@gmail.com>
2023-09-23 15:39:04 +0300

    Added FE code for Search functionality

--- src/Components/Home/NavBar.jsx
+++ src/Components/Home/NavBar.jsx
@@ -5,16 +5,52 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { MdAddCircleOutline } from "react-icons/md";
 import { IoCreateOutline } from "react-icons/io5";
 import { useEffect } from "react";
-import axios from "axios";
 import avatar from "../Assets/avatar.png"
 import Logout from "../Logout/Logout";
+import { makeAuthenticatedGETRequest } from "../Utils/ServerHelpers";
 
 const NavBar = () => {
 
     const [ searchText, setSearchText ] = useState("");
     const [ isOpen, setIsOpen ] = useState(false);
     const [openAvatar, setOpenAvatar] = useState(false);
-    const [dataUrl, setDataUrl] = useState('');
+    const [searchResults, setSearchResults] = useState({ mixes: [], users: [] });
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                if (searchText.trim() === "" ){
+                    setSearchResults({ mixes: [], users: []})
+                } else {
+                    const response = await makeAuthenticatedGETRequest(
+                        `/search/searchText?query=${searchText}`
+                    );
+                    console.log("API Response:", response.data);
+                    setSearchResults(response.data)  
+                }
+
+            } catch(error){
+                console.error("Error searching text", error);
+            }
+        }
+        fetchData();
+    }, [searchText]);
+
+    console.log("Search Results:", searchResults);
+
+    const handleSearchInputChange = (e) => {
+        setSearchText(e.target.value);
+      };
+
+      const handleSearchSubmit = (e) => {
+        e.preventDefault(); 
+       
+      };
+
+      if (searchResults === null) {
+        return <div>Loading...</div>;
+      };
 
     
 
@@ -31,13 +67,16 @@ const NavBar = () => {
                     </div>
                     <div className="flex items-center  px-5 bg-gray-200 space-x-3  ">
                         <AiOutlineSearch className="text-3xl font-semibold" />
+                        <form onSubmit={handleSearchSubmit}>
                         <input
-                        type="text"
-                        placeholder="Search"
-                        className="text-xl text-white px-3 py-2 w-full bg-gray-600 rounded-none rounded-b-md border border-gray-300 placeholder-white focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
-                        onChange={(e) => setSearchText(e.target.value)}
+                         type="text"
+                         placeholder="Search"
+                         className="text-xl text-white px-3 py-2 w-full bg-gray-600 rounded-none rounded-b-md border border-gray-300 placeholder-white focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+                         value={searchText}
+                         onChange={handleSearchInputChange}
                         
                         />
+                        </form>
                     </div>
                 </div>
                 <div className="relative flex flex-col items-center justify-center ">
@@ -86,11 +125,36 @@ const NavBar = () => {
                                 </li>
                             </ul>
                         </div>
-                    )
-                    }
-
+                    )}
                 </div>
             </div>
+            
+                  {/* Display search results */}
+      {searchResults.mixes.length > 0 && (
+        <div className="search-results">
+          <h3>Mixes</h3>
+          <ul>
+            {searchResults.mixes.map((mix) => (
+              <li key={mix._id}>
+                {mix.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {searchResults.users.length > 0 && (
+        <div className="search-results">
+          <h3>Users</h3>
+          <ul>
+            {searchResults.users.map((user) => (
+              <li key={user._id}>
+                {user.userName}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
         </nav>
     )
 }
