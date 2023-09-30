729db326f67228b6b85b02df717e74be17566c4e
Elvis Sunguti <elviskendrick99@gmail.com>
2023-09-30 16:52:35 +0300

    added FE code to fetch my mixes in the profile

--- src/Components/Upload/Uploads.jsx
+++ src/Components/Upload/Uploads.jsx
@@ -1,28 +1,222 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
+import { makeAuthenticatedGETRequest, makeAuthenticatedPOSTRequest } from "../Utils/ServerHelpers";
+import MixCard from "../shared/MixCard";
+import CurrentMix from "../shared/CurrentMix";
 
 const Uploads = () => {
 
-    const [ uploads, setUploads ] = useState({});
+    const [mixData, setMixData ] = useState([]);
+    const [currentMix, setCurrentMix] = useState(null);
+    const [currentlyPlayingMixId, setCurrentlyPlayingMixId] = useState(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await makeAuthenticatedGETRequest(
+                "/mix/get/myMix"
+            );
+            setMixData(response.data);
+        };
+        getData();
+    }, []);
+
+    const handleToggleFavourite = async (_id, isFavourite) => {
+        try {
+          if (isFavourite) {
+            await deleteFavourite(_id);
+          } else {
+            await addFavourite(_id);
+          }
+          // No need to fetch data again, just update the state with the changed data
+          setMixData((prevFeedData) =>
+            prevFeedData.map((item) =>
+              item._id === _id ? { ...item, isFavourite: !isFavourite } : item
+            )
+          );
+        } catch (error) {
+          console.error("Error toggling favourite:", error);
+        }
+      };
+    
+      const addFavourite = async (_id) => {
+        try {
+          const response = await makeAuthenticatedPOSTRequest(
+            "/mix/addFavourite",
+            { mixId: _id }
+          );
+    
+          if (response.error) {
+            console.error("Error adding to favourites:", response.error);
+          }
+        } catch (error) {
+          console.error("Error adding to favourites:", error);
+        }
+      };
+    
+      const deleteFavourite = async (_id) => {
+        try {
+          const response = await makeAuthenticatedPOSTRequest(
+            "/mix/deleteFavourite",
+            { mixId: _id }
+          );
+    
+          if (response.error) {
+            console.error("Error deleting from favourites:", response.error);
+          }
+        } catch (error) {
+          console.error("Error deleting from favourites:", error);
+        }
+      };
+
+
+      const handlePlayPause = (mixId) => {
+        if (currentlyPlayingMixId === mixId) {
+          // Pause the currently playing mix
+          setIsPlaying(false);
+          setCurrentlyPlayingMixId(null);
+        } else {
+          // Play the selected mix
+          setIsPlaying(true);
+          setCurrentlyPlayingMixId(mixId);
+          // Find the playing mix data from feedData and set it as the currentMix
+          const playingMix = mixData.find((item) => item._id === mixId);
+          setCurrentMix({
+            ...playingMix,
+            currentSong: "play",
+            currentTime: 0,
+          });
+        }
+      };
+
+      const playPrevMix = () => {
+        if (!currentMix) {
+          // If no mix is currently set as the currentMix, return or do nothing
+          return;
+        }
+      
+        const currentIndex = mixData.findIndex((item) => item._id === currentMix._id);
+      
+        if (currentIndex > 0) {
+          const prevIndex = currentIndex - 1;
+          const prevMix = mixData[prevIndex];
+      
+          if (prevMix) {
+            // Pause the current mix if it is playing
+            if (isPlaying) {
+              setIsPlaying(false);
+              const audioElement = document.getElementById(`audio-${currentMix._id}`);
+              if (audioElement) {
+                audioElement.pause();
+              }
+            }
+      
+            // Set the previous mix as the current mix and play it
+            setCurrentMix({
+              ...prevMix,
+              currentSong: "play", 
+              currentTime: 0,
+            });
+            setCurrentlyPlayingMixId(prevMix._id);
+            const audioElement = document.getElementById(`audio-${prevMix._id}`);
+              if (audioElement) {
+                audioElement.play();
+              }
+              setIsPlaying(true);
+          }
+        }
+      };
+      
+      const playNextMix = () => {
+        if (!currentMix) {
+          // If no mix is currently set as the currentMix, return or do nothing
+          return;
+        }
+      
+        const currentIndex = mixData.findIndex((item) => item._id === currentMix._id);
+      
+        if (currentIndex !== -1 && currentIndex < mixData.length - 1) {
+          const nextIndex = currentIndex + 1;
+          const nextMix = mixData[nextIndex];
+      
+          if (nextMix) {
+            // Pause the current mix if it is playing
+            if (isPlaying) {
+              setIsPlaying(false);
+              const audioElement = document.getElementById(`audio-${currentMix._id}`);
+              if (audioElement) {
+                audioElement.pause();
+              }
+            }
+      
+            // Set the next mix as the current mix and play it
+            setCurrentMix({
+              ...nextMix,
+              currentSong: "play", 
+              currentTime: 0,
+            });
+            setCurrentlyPlayingMixId(nextMix._id);
+            const audioElement = document.getElementById(`audio-${nextMix._id}`);
+            if (audioElement) {
+              audioElement.play();
+            }
+            setIsPlaying(true);
+          }
+        }
+      };
+
 
     return(
         <section>
-            <div>
-                <p>MY MIXES</p>
-                <div>
-                    { uploads.id && (
-                        <div>
-                            { uploads.map((index) => {
-                                return (
-                                    <div key={index}>
-                                        {uploads}
-                                    </div>    
-                                )
-                            })}
-                        </div>
-                    )}
-                </div>
-            </div>
+        <div className="flex items-start mb-6">
+            <h1 className="font-bold text-xl">MY MIXES</h1>
+        </div>
+        <div className="space-y-4 overflow-auto  ">
+            {mixData.length > 0 ? (
+              mixData.map((item, index) => (
+                 <MixCard
+                    key={index}  
+                    mixId={item._id}
+                    thumbnail={item.thumbnail}  
+                    title={item.title}
+                    artist={item.artist} 
+                    userId={item.userId}
+                    audioSrc={item.track}
+                    toggleFavourite={() =>
+                    handleToggleFavourite(item._id, item.isFavourite)
+                       }
+                    favouriteCount={item.favouriteCount}
+                    onMixPlay={handlePlayPause}
+                    currentlyPlayingMixId={currentlyPlayingMixId}
+                     isPlaying={isPlaying}
+                         />
+                ))
+                ) : (
+                 <p>Loading...</p>
+              )}
+        </div>
+        {currentMix && (
+          <CurrentMix
+          mixId={currentMix._id}
+          userId={currentMix.userId}
+          thumbnail={currentMix.thumbnail}
+          title={currentMix.title}
+          artist={currentMix.artist}
+          audioSrc={currentMix.track}
+          currentSong={currentMix.currentSong}
+          setCurrentSong={(songState) => 
+              setCurrentMix({...currentMix, currentMix: songState})
+          }
+          currentTime={currentMix.currentTime}
+          duration={currentMix.duration}
+          isPlaying={isPlaying}
+          onMixPlay={handlePlayPause}
+          onPlayNext={playNextMix} 
+          onPlayPrev={playPrevMix}
+
+          />
+        )}
+    
         </section>
     )
 }
